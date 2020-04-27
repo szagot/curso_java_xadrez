206e1993ba52d91c9d66aae62813333e502accbd
Daniel Bispo <szagot@gmail.com>
2020-04-27 16:56:43 -0300

    Criado tabuleiro

--- chess/src/application/Program.java
+++ chess/src/application/Program.java
@@ -1,14 +1,16 @@
 package application;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
-
+		ChessMatch chessMatch = new ChessMatch();
+		
+		// Imprime o tabuleiro
+		UI.printBoard(chessMatch.getPieces());
+		
 	}
 
 }
