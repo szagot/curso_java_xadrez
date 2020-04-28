25bcbe34b0ba199fb657448f5311c041b5b45a72
Daniel Bispo <szagot@gmail.com>
2020-04-28 11:39:46 -0300

    Melhoria no tabuleiro: imprimindo em cores

--- chess/src/application/UI.java
+++ chess/src/application/UI.java
@@ -1,13 +1,37 @@
 package application;
 
 import chess.ChessPiece;
+import chess.Color;
 
 /**
  * Classe com métodos auxiliares para o programa de xadrez
+ * 
+ * Cores no console do JAVA:
+ * https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+ * 
  * @author szago
  *
  */
 public class UI {
+	// Constantes de cores de letras
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+	// Constantes de cores de fundo
+	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+	public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+	public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
 	/**
 	 * Iprime o tabuleiro
@@ -26,11 +50,11 @@ public class UI {
 				// Imprime a peça
 				printPiece(pieces[i][j]);
 			}
-			
+
 			// Terminando a linha, quebra a linha
 			System.out.println();
 		}
-		
+
 		// Imprime as letras das colunas
 		System.out.println("  a b c d e f g h");
 
@@ -42,13 +66,20 @@ public class UI {
 	 * @param piece
 	 */
 	private static void printPiece(ChessPiece piece) {
-		// Tina peça nessa posição?
+		// Tinha peça nessa posição?
 		if (piece == null) {
 			System.out.print("-");
 		} else {
-			System.out.print(piece);
+			// A peça é branca?
+			if (piece.getColor() == Color.WHITE) {
+				// Imprime em ciano
+				System.out.print(ANSI_CYAN + piece + ANSI_RESET);
+			} else {
+				// Imprime em roxo
+				System.out.print(ANSI_PURPLE + piece + ANSI_RESET);
+			}
 		}
-
+		
 		System.out.print(" ");
 	}
 }
